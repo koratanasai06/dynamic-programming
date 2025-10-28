91e7659e27e3885243655a9f87dffdf31cf250d7
koratana sai <koratanasai03@gmail.com>
2025-10-28 13:16:01 +0530

    Refactor BestTimeToBuyShell2 for multiple approaches

--- BestTimeToBuyShell2.java
+++ BestTimeToBuyShell2.java
@@ -30,3 +30,58 @@ public class BestTimeToBuyShell2 {
         return memo[buy][i]=profit;
     }
 }
+
+
+
+
+package bottomup;
+
+import java.util.Arrays;
+
+public class BestTimeToBuyShell2 {
+    public static void main(String[] args) {
+        int[] nums=new int[]{7,1,5,3,6,4};
+        int n=nums.length;
+        int[][] dp = new int[2][n+1];
+        dp[0][nums.length]=dp[1][n]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                int profit=Integer.MIN_VALUE;
+                if(buy==0){
+                    profit=Math.max(-nums[i]+dp[1][i+1],dp[0][i+1]);
+                }else{
+                    profit=Math.max(nums[i]+dp[0][i+1],dp[1][i+1]);
+                }
+                dp[buy][i]=profit;
+            }
+        }
+        System.out.println(Arrays.deepToString(dp));
+        System.out.println(dp[0][0]);
+    }
+}
+
+
+
+package spaceoptimization;
+
+public class BestTimeToBuyShell2 {
+    public static void main(String[] args) {
+        int[] nums=new int[]{7,1,5,3,6,4};
+        int n=nums.length;
+        int[] sodp = new int[2];
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                int profit=Integer.MIN_VALUE;
+                if(buy==0){
+                    profit=Math.max(-nums[i]+sodp[1],sodp[0]);
+                }else{
+                    profit=Math.max(nums[i]+sodp[0],sodp[1]);
+                }
+                sodp[buy]=profit;
+            }
+        }
+        System.out.println(sodp[0]);
+    }
+}
+
+
