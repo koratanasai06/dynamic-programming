979892ff3dd897b3a03396e71ace9f14d3b83c53
koratana sai <koratanasai03@gmail.com>
2025-10-28 13:22:35 +0530

    Implement bottom-up and space optimization solutions
    Added bottom-up and space optimization implementations for the best time to buy and sell stock with cooldown.

--- BestTimeShellBuyCoolDown.java
+++ BestTimeShellBuyCoolDown.java
@@ -25,3 +25,53 @@ public class BestTimeShellBuyCoolDown {
         }
     }
 }
+
+
+
+
+
+package bottomup;
+
+public class BestTimeShellBuyCoolDown {
+
+    public static void main(String[] args) {
+        int[] p=new int[]{1,2,3,0,2};
+        int[][] dp=new int[p.length+2][2];
+        for(int i=p.length-1;i>=0;i--){
+            for(int b=0;b<2;b++){
+                if(b==0){
+                    dp[i][b]=Math.max(-p[i]+dp[i+1][1],dp[i+1][0]);
+                }else{
+                    dp[i][b]=Math.max(p[i]+dp[i+2][0],dp[i+1][1]);
+                }
+            }
+        }
+        System.out.println(dp[0][0]);
+    }
+}
+
+
+
+
+
+package spaceoptimization;
+
+public class BestTimeShellBuyCoolDown {
+
+    public static void main(String[] args) {
+        int[] p=new int[]{1,2,3,0,2};
+        int[][] dp=new int[p.length+2][2];
+        for(int i=p.length-1;i>=0;i--){
+            for(int b=0;b<2;b++){
+                if(b==0){
+                    dp[i][b]=Math.max(-p[i]+dp[i+1][1],dp[i+1][0]);
+                }else{
+                    dp[i][b]=Math.max(p[i]+dp[i+2][0],dp[i+1][1]);
+                }
+            }
+        }
+        System.out.println(dp[0][0]);
+    }
+}
+
+
